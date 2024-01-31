5438276488beae239ba05eff2201400f2e0c6afc
bungongtoto <kingsley.bungong@ictuniversity.edu.cm>
2024-01-31 08:46:12 +0100

    completed

--- frontend/src/components/home/TaskModal.jsx
+++ frontend/src/components/home/TaskModal.jsx
@@ -1,9 +1,9 @@
 import { FcTodoList } from "react-icons/fc";
-import { AiOutlineClose } from "react-icons/ai";
 import { MdTitle } from "react-icons/md";
 import PropTypes from "prop-types";
 
 const TaskModal = ({ task,onClose }) => {
+  const completed = task.completed;
   return (
     <div
       className="fixed bg-black bg-opacity-60 top-0 left-0 right-0 bottom-0 z-50 flex justify-center items-center"
@@ -15,11 +15,7 @@ const TaskModal = ({ task,onClose }) => {
         }}
         className="w-[600px] max-w-full max-h-[400px] bg-white rounded-xl p-4 flex flex-col relative"
       >
-        <AiOutlineClose
-          className="absolute right-6 top-6 text-3xl text-red-600 cursor-pointer"
-          onClick={onClose}
-        />
-        <h2 className="w-fit px-4 py-1 bg-red-300 rounded-lg text-white">
+        <h2 className={ completed ? "absolute top-1 right-2 px-4 py-1 rounded-lg text-white bg-green-400" : "absolute top-1 right-2 px-4 py-1 rounded-lg text-white bg-red-300"}>
           {task.completed ? "Complted" : "Active"}
         </h2>
         
